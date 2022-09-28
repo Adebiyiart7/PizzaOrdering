5bb54b3b3cad2a72e433c40129d41a042e06f714
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-09-29 00:03:04 +0100

    Created the Card component

--- App.js
+++ App.js
@@ -2,31 +2,73 @@ import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Screen from "./app/components/Screen";
-import CategoryIcon from "./app/components/CategoryIcon";
+import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "./app/config/colors";
 
 export default function App() {
-  const [text, setText] = useState("");
-  console.log(text);
   return (
-    <Screen style={{ paddingHorizontal: 16, flex: 1, flexDirection: "row" }}>
-      <CategoryIcon
-        label="Pizza"
-        image={require("./app/assets/dessert-04.png")}
-      />
-      <CategoryIcon
-        label="Pizza"
-        image={require("./app/assets/dessert-04.png")}
-      />
-      <CategoryIcon
-        label="Pizza"
-        image={require("./app/assets/dessert-04.png")}
-      />
-      <CategoryIcon
-        label="Pizza"
-        image={require("./app/assets/dessert-04.png")}
+    <Screen style={{ paddingHorizontal: 16, flex: 1 }}>
+      <Card
+        onPress={() => console.log("Hello")}
+        image={require("./app/assets/img-01.jpg")}
+        title="Australian Pizza"
+        subTitle="Our famous meat pizza is topped"
+        leftInfo={
+          <>
+            <MaterialCommunityIcons
+              name="alarm"
+              size={16}
+              color={colors.mediumGray}
+            />
+            <Text>&nbsp;25-30min</Text>
+          </>
+        }
+        rightInfo={
+          <>
+            <MaterialCommunityIcons
+              name="star-outline"
+              size={16}
+              color={colors.mediumGray}
+              style={{ marginRight: 8 }}
+            />
+            <Text>&nbsp;4.8</Text>
+          </>
+        }
       />
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({});
+
+/**
+ *           <Card
+        onPress={() => console.log("Hello")}
+        image={require("./app/assets/img-01.jpg")}
+        title="Australian Pizza"
+        subTitle="Our famous meat pizza is topped"
+        leftInfo={
+          <>
+            <MaterialCommunityIcons
+              name="alarm"
+              size={16}
+              color={colors.mediumGray}
+            />
+            <Text>&nbsp;25-30min</Text>
+          </>
+        }
+        rightInfo={
+          <>
+            <MaterialCommunityIcons
+              name="star-outline"
+              size={16}
+              color={colors.mediumGray}
+              style={{ marginRight: 8 }}
+            />
+            <Text>&nbsp;4.8</Text>
+          </>
+        }
+      />
+ */
