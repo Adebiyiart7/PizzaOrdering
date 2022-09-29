26f0db61a73584cfac616df99d8559c60614c406
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-09-29 05:54:55 +0100

    Created a trash folder

--- App.js
+++ App.js
@@ -2,41 +2,12 @@ import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Screen from "./app/components/Screen";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import colors from "./app/config/colors";
+import Chip from "./app/components/Chip";
 
 export default function App() {
   return (
     <Screen style={{ paddingHorizontal: 16, flex: 1 }}>
-      <Card
-        onPress={() => console.log("Hello")}
-        image={require("./app/assets/img-01.jpg")}
-        title="Australian Pizza"
-        subTitle="Our famous meat pizza is topped"
-        leftInfo={
-          <>
-            <MaterialCommunityIcons
-              name="alarm"
-              size={16}
-              color={colors.mediumGray}
-            />
-            <Text>&nbsp;25-30min</Text>
-          </>
-        }
-        rightInfo={
-          <>
-            <MaterialCommunityIcons
-              name="star-outline"
-              size={16}
-              color={colors.mediumGray}
-              style={{ marginRight: 8 }}
-            />
-            <Text>&nbsp;4.8</Text>
-          </>
-        }
-      />
+      <Chip />
     </Screen>
   );
 }
---
+++ app/components/Chip.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Chip = () => {
+  return (
+    <View>
+      <Text>Chip</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Chip;
