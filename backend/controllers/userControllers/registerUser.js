acafa2fbf41aa7a75bb05bfed0e125556d4cdf5d
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2022-10-01 15:07:28 +0100

    Completed register user controller

--- backend/controllers/userControllers/registerUser.js
+++ backend/controllers/userControllers/registerUser.js
@@ -1,9 +1,34 @@
 const asyncHandler = require("express-async-handler");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const User = require("../../models/userModel");
+const generateToken = require("../../utils/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register user" });
+  const { email, phone_number, password } = req.body;
+
+  if (!email || !phone_number || !password) {
+    res.status(400);
+    throw new Error("Please fill in all fields.");
+  }
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exist.");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    email,
+    phone_number,
+    password: hashedPassword,
+  });
+
+  res.status(200).json({ id: user.id, token: generateToken(user.id) });
 });
 
 module.exports = registerUser;
